a77c7ca4e5a5ca0e453252ddbfcd7b1d7524f8d2
Cj-bc <cj-bc@y-modify.org>
2018-09-19 22:54:27 +0900

    Add: util> 'launsh.util.error' added

---
+++ lib/util.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# @(#) laun.sh.key  -- key related library for laun.sh
+#
+# @(#) copyright (c) 2018 Cj-bc
+# @(#) this software is released under MIT License
+#
+# @(#) ver: 0.0.1
+
+
+# return error code based on error name
+# @param <str error_name>
+# @return <int error_code>
+function launsh.util.error {
+
+}
