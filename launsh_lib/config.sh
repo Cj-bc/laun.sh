cf2e485ffcf6f268e6bb01ed8f6ca13dd1d7ab99
Cj-bc <cj-bc@y-modify.org>
2018-09-30 21:37:23 +0900

    Change: rewrite config, key

---
+++ launsh_lib/config.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# @(#) laun.sh.config  -- config library for laun.sh
+#
+# @(#) copyright (c) 2018 Cj-bc
+# @(#) this software is released under MIT License
+#
+# @(#) ver: 0.0.1
+
+# bash-oo-framework
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+
+import util/log util/exception util/tryCatch util/namedParameters util/class
+
+namespace LaunshConfig
+
+Log::AddOutput LaunshConfig DEBUG
+
+class:LaunshConfig() {
+  public string conf_path
+
+  # initialize configs.
+  # load launsh.yaml and set values for Key
+  LaunshConfig.__init__() {
+    [string] conf_path
+
+    for key in {A-Z}; do
+      src=$(cat $conf_path | shyaml get-value binds.${key}.src)
+      attr=$(cat $conf_path | shyaml get-value binds.${key}.attr)
+      Key $key
+      var: $key __init__ $src $attr
+      success=$?
+      subject="gen_key" Log "Key generated($success): $key"
+    done
+  }
+}
---
+++ launsh_lib/key.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# @(#) laun.sh.key  -- key related library for laun.sh
+#
+# @(#) copyright (c) 2018 Cj-bc
+# @(#) this software is released under MIT License
+#
+# @(#) ver: 0.0.1
+
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+
+import util config
+
+class:Key() {
+  private string src
+  private string attr
+  private integer prev_pid
+
+  Key.__init__() {
+    [string] src
+    [string] attr
+
+    this attr = $attr
+    this src = $src
+    this played = false
+  }
+  # get attribute for <key>.
+  # could be ['clip', 'shot']
+  # @return <str attr>
+  Key.get_attr() {
+    @return this attr
+  }
+
+  # get clip file path binded to <key>
+  # @param <str key>
+  # @return <str clip_path>
+  Key.get_src() {
+    @return this src
+  }
+
+  key.get_prevPid() {
+    @return this prev_pid
+  }
+
+  # set prev_pid
+  # @param <int pid>
+  key.set_prePid() {
+    [integer] pid
+    this prev_pid = $pid
+  }
+
+  # return available attributes
+  # @return $EX_OK(0) list of available attributes
+  Key::available_attr() {
+    [array] attrs = ("trigger" "shot")
+
+    @return attrs
+  }
+}
